2c736800cc83ffcc404742d8ad8875678c87b34c
hiteshwarkaim <hemant.kaimwal96@gmail.com>
2023-05-06 18:00:25 +0530

    store created

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import { legacy_createStore as createStore } from 'redux';
+import movies from './reducers';
+
+const store=createStore(movies);
+console.log('store',store);
+console.log('state',store.getState());
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
