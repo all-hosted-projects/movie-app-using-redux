80856218eddcf95fa6d4729d6ab6fed46780132b
hiteshwarkaim <hemant.kaimwal96@gmail.com>
2023-05-06 18:06:34 +0530

    dispatch action to reducer

--- src/index.js
+++ src/index.js
@@ -7,8 +7,14 @@ import movies from './reducers';
 
 const store=createStore(movies);
 console.log('store',store);
-console.log('state',store.getState());
+console.log('before state',store.getState());
 
+store.dispatch({
+  type:'ADD_MOVIES',
+  movies:[{name:'superman'}]
+});
+
+console.log('after state',store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
